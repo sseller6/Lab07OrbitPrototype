4095fe70d6f720b536493556d49decd108fa9231
Pingpong403 <pingpong440033@gmail.com>
2024-02-21 16:26:18 -0700

    Isolated the GPS.

--- Lab07.cpp
+++ Lab07.cpp
@@ -27,22 +27,22 @@ public:
    Demo(Position ptUpperRight) :
       ptUpperRight(ptUpperRight)
    {
-      ptHubble.setPixelsX(ptUpperRight.getPixelsX() * random(-0.5, 0.5));
-      ptHubble.setPixelsY(ptUpperRight.getPixelsY() * random(-0.5, 0.5));
+//      ptHubble.setPixelsX(ptUpperRight.getPixelsX() * random(-0.5, 0.5));
+//      ptHubble.setPixelsY(ptUpperRight.getPixelsY() * random(-0.5, 0.5));
+//
+//      ptSputnik.setPixelsX(ptUpperRight.getPixelsX() * random(-0.5, 0.5));
+//      ptSputnik.setPixelsY(ptUpperRight.getPixelsY() * random(-0.5, 0.5));
+//
+//      ptStarlink.setPixelsX(ptUpperRight.getPixelsX() * random(-0.5, 0.5));
+//      ptStarlink.setPixelsY(ptUpperRight.getPixelsY() * random(-0.5, 0.5));
+//
+//      ptCrewDragon.setPixelsX(ptUpperRight.getPixelsX() * random(-0.5, 0.5));
+//      ptCrewDragon.setPixelsY(ptUpperRight.getPixelsY() * random(-0.5, 0.5));
+//
+//      ptShip.setPixelsX(ptUpperRight.getPixelsX() * random(-0.5, 0.5));
+//      ptShip.setPixelsY(ptUpperRight.getPixelsY() * random(-0.5, 0.5));
 
-      ptSputnik.setPixelsX(ptUpperRight.getPixelsX() * random(-0.5, 0.5));
-      ptSputnik.setPixelsY(ptUpperRight.getPixelsY() * random(-0.5, 0.5));
-
-      ptStarlink.setPixelsX(ptUpperRight.getPixelsX() * random(-0.5, 0.5));
-      ptStarlink.setPixelsY(ptUpperRight.getPixelsY() * random(-0.5, 0.5));
-
-      ptCrewDragon.setPixelsX(ptUpperRight.getPixelsX() * random(-0.5, 0.5));
-      ptCrewDragon.setPixelsY(ptUpperRight.getPixelsY() * random(-0.5, 0.5));
-
-      ptShip.setPixelsX(ptUpperRight.getPixelsX() * random(-0.5, 0.5));
-      ptShip.setPixelsY(ptUpperRight.getPixelsY() * random(-0.5, 0.5));
-
-      ptGPS.setPixelsX(ptUpperRight.getPixelsX() * random(-0.5, 0.5));
+      ptGPS.setPixelsX(0);
       ptGPS.setPixelsY(ptUpperRight.getPixelsY() * random(-0.5, 0.5));
 
       ptStar.setPixelsX(ptUpperRight.getPixelsX() * random(-0.5, 0.5));
@@ -53,11 +53,11 @@ public:
       phaseStar = 0;
    }
 
-   Position ptHubble;
-   Position ptSputnik;
-   Position ptStarlink;
-   Position ptCrewDragon;
-   Position ptShip;
+//   Position ptHubble;
+//   Position ptSputnik;
+//   Position ptStarlink;
+//   Position ptCrewDragon;
+//   Position ptShip;
    Position ptGPS;
    Position ptStar;
    Position ptUpperRight;
@@ -86,14 +86,14 @@ void callBack(const Interface* pUI, void* p)
    //
 
    // move by a little
-   if (pUI->isUp())
-      pDemo->ptShip.addPixelsY(1.0);
-   if (pUI->isDown())
-      pDemo->ptShip.addPixelsY(-1.0);
-   if (pUI->isLeft())
-      pDemo->ptShip.addPixelsX(-1.0);
-   if (pUI->isRight())
-      pDemo->ptShip.addPixelsX(1.0);
+//   if (pUI->isUp())
+//      pDemo->ptShip.addPixelsY(1.0);
+//   if (pUI->isDown())
+//      pDemo->ptShip.addPixelsY(-1.0);
+//   if (pUI->isLeft())
+//      pDemo->ptShip.addPixelsX(-1.0);
+//   if (pUI->isRight())
+//      pDemo->ptShip.addPixelsX(1.0);
 
 
    //
@@ -113,34 +113,35 @@ void callBack(const Interface* pUI, void* p)
    ogstream gout(pt);
 
    // draw satellites
-   gout.drawCrewDragon(pDemo->ptCrewDragon, pDemo->angleShip);
-   gout.drawHubble    (pDemo->ptHubble,     pDemo->angleShip);
-   gout.drawSputnik   (pDemo->ptSputnik,    pDemo->angleShip);
-   gout.drawStarlink  (pDemo->ptStarlink,   pDemo->angleShip);
-   gout.drawShip      (pDemo->ptShip,       pDemo->angleShip, pUI->isSpace());
+//   gout.drawCrewDragon(pDemo->ptCrewDragon, pDemo->angleShip);
+//   gout.drawHubble    (pDemo->ptHubble,     pDemo->angleShip);
+//   gout.drawSputnik   (pDemo->ptSputnik,    pDemo->angleShip);
+//   gout.drawStarlink  (pDemo->ptStarlink,   pDemo->angleShip);
+//   gout.drawShip      (pDemo->ptShip,       pDemo->angleShip, pUI->isSpace());
+   
    gout.drawGPS       (pDemo->ptGPS,        pDemo->angleShip);
 
    // draw parts
-   pt.setPixelsX(pDemo->ptCrewDragon.getPixelsX() + 20);
-   pt.setPixelsY(pDemo->ptCrewDragon.getPixelsY() + 20);
-   gout.drawCrewDragonRight(pt, pDemo->angleShip); // notice only two parameters are set
-   pt.setPixelsX(pDemo->ptHubble.getPixelsX() + 20);
-   pt.setPixelsY(pDemo->ptHubble.getPixelsY() + 20);
-   gout.drawHubbleLeft(pt, pDemo->angleShip);      // notice only two parameters are set
-   pt.setPixelsX(pDemo->ptGPS.getPixelsX() + 20);
-   pt.setPixelsY(pDemo->ptGPS.getPixelsY() + 20);
-   gout.drawGPSCenter(pt, pDemo->angleShip);       // notice only two parameters are set
-   pt.setPixelsX(pDemo->ptStarlink.getPixelsX() + 20);
-   pt.setPixelsY(pDemo->ptStarlink.getPixelsY() + 20);
-   gout.drawStarlinkArray(pt, pDemo->angleShip);   // notice only two parameters are set
+//   pt.setPixelsX(pDemo->ptCrewDragon.getPixelsX() + 20);
+//   pt.setPixelsY(pDemo->ptCrewDragon.getPixelsY() + 20);
+//   gout.drawCrewDragonRight(pt, pDemo->angleShip); // notice only two parameters are set
+//   pt.setPixelsX(pDemo->ptHubble.getPixelsX() + 20);
+//   pt.setPixelsY(pDemo->ptHubble.getPixelsY() + 20);
+//   gout.drawHubbleLeft(pt, pDemo->angleShip);      // notice only two parameters are set
+//   pt.setPixelsX(pDemo->ptGPS.getPixelsX() + 20);
+//   pt.setPixelsY(pDemo->ptGPS.getPixelsY() + 20);
+//   gout.drawGPSCenter(pt, pDemo->angleShip);       // notice only two parameters are set
+//   pt.setPixelsX(pDemo->ptStarlink.getPixelsX() + 20);
+//   pt.setPixelsY(pDemo->ptStarlink.getPixelsY() + 20);
+//   gout.drawStarlinkArray(pt, pDemo->angleShip);   // notice only two parameters are set
 
    // draw fragments
-   pt.setPixelsX(pDemo->ptSputnik.getPixelsX() + 20);
-   pt.setPixelsY(pDemo->ptSputnik.getPixelsY() + 20);
-   gout.drawFragment(pt, pDemo->angleShip);
-   pt.setPixelsX(pDemo->ptShip.getPixelsX() + 20);
-   pt.setPixelsY(pDemo->ptShip.getPixelsY() + 20);
-   gout.drawFragment(pt, pDemo->angleShip);
+//   pt.setPixelsX(pDemo->ptSputnik.getPixelsX() + 20);
+//   pt.setPixelsY(pDemo->ptSputnik.getPixelsY() + 20);
+//   gout.drawFragment(pt, pDemo->angleShip);
+//   pt.setPixelsX(pDemo->ptShip.getPixelsX() + 20);
+//   pt.setPixelsY(pDemo->ptShip.getPixelsY() + 20);
+//   gout.drawFragment(pt, pDemo->angleShip);
 
    // draw a single star
    gout.drawStar(pDemo->ptStar, pDemo->phaseStar);
